80ee80af6423b2607676d4fbc9bf0d69c436978a
Nico Viarnes <nico@viarn.es>
2023-06-28 08:22:15 -0700

    Initial commit

---
+++ audio_examples.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# call audio manager singletons
+# example: manager.play(SOUND_FILE_PATH, DECIBEL_VOLUME)
+
+# MusicManager.play(SOUND_FILE_PATH, -35)
+# SfxManager.play(SOUND_FILE_PATH, -35)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
